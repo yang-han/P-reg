3b51d3739a54cf9f9e67179e0da4cc32a572efe8
klma <klma@cse.cuhk.edu.hk>
2020-05-20 15:54:32 +0800

    script for running experiment. TODO: split laplacian.sh base don datasets

--- scripts/gcn_label.sh
+++ scripts/gcn_label.sh
@@ -1,7 +1,7 @@
 datasets="cora citeseer pubmed cs physics computers photo"
 model="gcn"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
-RESULT_DIR=$DIR$"/../result/"$model
+RESULT_DIR=$DIR$"/../result/"$model$"_label"
 echo $RESULT_DIR
 
 if [ ! -d "$RESULT_DIR"  ]; then
@@ -10,9 +10,10 @@ fi
 
 path=$DIR$"/../config/"
 config_path=$path$model$".json"
-gpu=0
-
+gpu=1
+smoothings="0.0001 0.001 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.99"
 for dataset in ${datasets}; do
+  for smoothing in ${smoothings}; do
     lr=$(jq .$dataset.lr $config_path)
     weight_decay=$(jq .$dataset.weight_decay $config_path)
     patience=$(jq .$dataset.patience $config_path)
@@ -21,9 +22,10 @@ for dataset in ${datasets}; do
     epochs=$(jq .$dataset.epochs $config_path)
     hidden_size=$(jq .$dataset.hidden_size $config_path)
     echo "running $dataset"
-    python ../main_vanilla.py --dataset $dataset --model $model --gpu $gpu \
+    python ../main_label.py --dataset $dataset --model $model --gpu $gpu \
       --epochs $epochs --num_seeds $num_seeds --num_splits $num_splits --patience $patience \
-      --hidden_size $hidden_size --lr $lr --weight_decay $weight_decay
+      --hidden_size $hidden_size --lr $lr --weight_decay $weight_decay --regularizer $smoothing
+  done
 done
 #path=$DIR$"/../config/"
 #config_path=$path$model$".json"
--- scripts/laplacian.sh
+++ scripts/laplacian.sh
@@ -1,5 +1,5 @@
 datasets="cora citeseer pubmed cs physics computers photo"
-model="gcn"
+model="iadgcn"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 RESULT_DIR=$DIR$"/../result/"$model
 echo $RESULT_DIR
@@ -10,9 +10,10 @@ fi
 
 path=$DIR$"/../config/"
 config_path=$path$model$".json"
-gpu=0
-
+gpu=2
+mus="0.0001 0.001 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.99"
 for dataset in ${datasets}; do
+  for mu in ${mus}; do
     lr=$(jq .$dataset.lr $config_path)
     weight_decay=$(jq .$dataset.weight_decay $config_path)
     patience=$(jq .$dataset.patience $config_path)
@@ -21,9 +22,10 @@ for dataset in ${datasets}; do
     epochs=$(jq .$dataset.epochs $config_path)
     hidden_size=$(jq .$dataset.hidden_size $config_path)
     echo "running $dataset"
-    python ../main_vanilla.py --dataset $dataset --model $model --gpu $gpu \
+    python ../main_laplacian.py --dataset $dataset --model $model --gpu $gpu \
       --epochs $epochs --num_seeds $num_seeds --num_splits $num_splits --patience $patience \
-      --hidden_size $hidden_size --lr $lr --weight_decay $weight_decay
+      --hidden_size $hidden_size --lr $lr --weight_decay $weight_decay --mu $mu
+  done
 done
 #path=$DIR$"/../config/"
 #config_path=$path$model$".json"
